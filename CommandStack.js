2ff113d03f11946722dd36cba4ef0f9603328b32
Logan Kearsley <chronosurfer@gmail.com>
2013-02-19 13:03:57 -0700

    Undo/Redo keys & track saved state
    Added keybindings of ctrl+z and ctrl+y shortcuts and a mechanism for
    tracking the most recently saved state of an editor on the undo/redo
    stack.

--- CommandStack.js
+++ CommandStack.js
@@ -4,7 +4,8 @@
 	}
 	EditorWidgets.CommandStack = function() {
 		var unstack = [],
-			restack = [];
+			restack = [],
+			saveIndex = 0;
 		/* Command object structure:
 		 * context: the value of 'this' for undo/redo calls
 		 * undo: the function to call to undo
@@ -15,6 +16,9 @@
 		this.push = function(com){
 			unstack.push(com);
 			restack = []; //erase redo stack
+			if(saveIndex >= unstack.length){ //check if a saved state is reachable
+				saveIndex = -1;
+			}
 		};
 		this.undo = function(){
 			var c;
@@ -36,11 +40,39 @@
 			function f(e){ return e.file !== name; }
 			unstack = unstack.filter(f);
 			restack = restack.filter(f);
-		}
+		};
 		this.renameEvents = function(o,n){
 			function f(e){ if(e.file === o){ e.file = n; } }
 			unstack.forEach(f);
 			restack.forEach(f);
-		}
+		};
+		Object.defineProperties(this,{
+			undoDepth: {get:function(){ return unstack.length; },enumerable:true},
+			redoDepth: {get:function(){ return restack.length; },enumerable:true},
+			saved: {
+				get: function(){ return unstack.length === saveIndex; },
+				set: function(v){
+					var b = !!v;
+					saveIndex = b?unstack.length:-1;
+					return b;
+				},
+				enumerable:true}
+		});
+	};
+	
+	EditorWidgets.CommandStack.prototype.bindKeyEvents = function(el){
+		var that = this;
+		el.addEventListener("keydown",function(e){
+			e = e||window.event;
+			if(!e.ctrlKey){ return; }
+			switch(e.keyCode){
+			case 89: that.redo();
+				e.preventDefault();
+				break;
+			case 90: that.undo();
+				e.preventDefault();
+				break;
+			}
+		},false);
 	};
 })();
